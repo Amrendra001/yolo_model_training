deea497155ee742ec7ed02ea9df71440ca31b4c4
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-07-14 09:09:51 +0530

    Update

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import wandb
 from ultralytics import YOLO
 import os
 
@@ -14,5 +13,5 @@ if __name__ == '__main__':
         s3_sync(s3_image_path, local_image_path)
 
         model = YOLO("yolov8m.pt")
-        model.train(data="data.yaml", epochs=1, save_period=10, val=True, project='Yolov8m_traning_cropped_column_data', batch=32, name='Training', cache='ram', close_mosaic=10, single_cls=True)
+        model.train(data="data.yaml", epochs=1, save_period=10, val=True, project='Localisation_auto_training', batch=32, cache='ram', close_mosaic=10, single_cls=True)
         model.val(data="data.yaml")
