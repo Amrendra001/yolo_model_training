a0ac46c335239667bc1ec499a0f7a56624adc4c1
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-07-14 10:58:02 +0530

    Update

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ if __name__ == '__main__':
         s3_sync(s3_image_path, local_image_path)
 
         model = YOLO("yolov8m.pt")
-        model.train(data="data.yaml", epochs=1, save_period=10, val=True, project='Localisation_auto_training', batch=32, cache='ram', close_mosaic=10, single_cls=True)
+        model.train(data="data.yaml", epochs=1, save_period=10, val=True, project='Localisation_auto_training', name='Training', batch=32, cache='ram', close_mosaic=10, single_cls=True)
         model.val(data="data.yaml")
