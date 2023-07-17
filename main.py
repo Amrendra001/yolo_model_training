f648032222a9b09111733c7e7c5bd2b8f9cb7779
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-07-17 20:36:46 +0530

    Update

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def s3_sync(source, destination):
 
 
 if __name__ == '__main__':
-        s3_image_path = 's3://document-ai-training-data/training_data/table_localisation/column/auto_training_testing_data/'
-        local_image_path = 'cropped_column_data/'
+        s3_image_path = 's3://document-ai-training-data/training_data/table_localisation/column/png_jpg_data/'
+        local_image_path = 'png_jpg_data/'
         s3_sync(s3_image_path, local_image_path)
 
         model = YOLO("yolov8m.pt")
-        model.train(data="data.yaml", epochs=1, save_period=10, val=True, project='Localisation_auto_training', batch=32, cache='ram', close_mosaic=10, single_cls=True)
+        model.train(data="data.yaml", epochs=75, save_period=10, val=True, project='Training', batch=32, cache='ram', close_mosaic=10, single_cls=True)
         model.val(data="data.yaml")
