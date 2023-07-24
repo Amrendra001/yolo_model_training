1ce0c46f1b80477484bf17f4ec417a160ff683d6
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-07-24 15:26:20 +0530

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import wandb
 import os
-from ultralytics import RTDETR
+from ultralytics import RTDETR, YOLO
 
 def s3_sync(source, destination):
     sync_command = f"aws s3 sync {source} {destination}"
@@ -12,5 +12,6 @@ if __name__ == '__main__':
         local_image_path = 'base_data/'
         s3_sync(s3_image_path, local_image_path)
 
-        model = RTDETR("rtdetr-l.pt")
+        # model = RTDETR("rtdetr-l.pt")
+        model = YOLO('yolov8m.pt')
         model.train(data="data.yaml", epochs=3, batch=32, single_cls=True, cache=True)
