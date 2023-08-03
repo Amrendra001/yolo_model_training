57916b428b9db07ffbeaee69cd86999e1f506ec2
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-03 23:10:14 +0530

    Update

--- test_saved_model.py
+++ test_saved_model.py
@@ -5,7 +5,7 @@ from testing import localisation_inference
 
 if __name__ == '__main__':
     project_name = 'Testing'
-    training_name = '500_epochs'
+    training_name = 'imgsz3'
 
     ls = glob(f'{project_name}/{training_name}/weights/*.pt')
     for path in ls:
