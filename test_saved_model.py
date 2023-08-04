24a800d6a0cede766e38ac8c7f71808dd69f9231
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-04 19:56:31 +0530

    Update

--- test_saved_model.py
+++ test_saved_model.py
@@ -1,35 +1,14 @@
 from glob import glob
 from ultralytics import YOLO
 from testing import localisation_inference
-from train import training
-import os
-from testing import download_test_data
-
-def s3_sync(source, destination):
-    sync_command = f"aws s3 sync {source} {destination}"
-    os.system(sync_command)
-
-
-def download_training_set():
-    s3_image_path = 's3://document-ai-training-data/training_data/table_localisation/column/new_smaller_training_data/'
-    local_image_path = 'new_smaller_training_data/'
-    s3_sync(s3_image_path, local_image_path)
 
 
 if __name__ == '__main__':
 
     project_name = 'Testing'
-    training_name = 'imgsz_testing'
-
-    # download_training_set()
-    download_test_data()
-
-    project_name = 'Testing'
     training_name = 'imgsz3'
 
-    # ls = glob(f'{project_name}/{training_name}/weights/*.pt')
-    # for path in ls:
-    #     model = YOLO(path)
-    #     localisation_inference(model, path, training_name)
-    model = YOLO('yolo_v8m_jpg_png.pt')
-    localisation_inference(model, project_name, training_name)
+    ls = glob(f'{project_name}/{training_name}/weights/*.pt')
+    for path in ls:
+        model = YOLO(path)
+        localisation_inference(model, path, training_name)
