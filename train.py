6979f932af8d096bf602b60e74dc1992354296c6
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-04 10:05:12 +0530

    Update

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 # import wandb
-from ultralytics import YOLO, RTDETR
+from ultralytics import YOLO
 from testing import localisation_inference
 
 
 def training(params, training_name):
-    model = RTDETR('rtdetr-l.pt')
+    model = YOLO('yolov8s.pt')
     model.train(**params)
     localisation_inference(model, params, training_name)
