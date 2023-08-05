8be61a3ae70fa633f1be1862ee7456f6f6504346
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-05 22:10:17 +0530

    Update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def download_training_set():
 if __name__ == '__main__':
 
     project_name = 'Testing_new'
-    training_name = 'augment'
+    training_name = 'pose'
 
     download_training_set()
     download_test_data()
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         'name': training_name,
         'lr0': 0.001,
         'lrf': 0.1,
-        'augment': True,
+        'pose': 0.5,
     }
     training(params, training_name)
     # os.system(f'rm -r {project_name}')
