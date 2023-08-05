309deeb59b1ee14d5ec7f4145a815c595b68316a
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-05 22:47:50 +0530

    Update

--- train.py
+++ train.py
@@ -6,4 +6,4 @@ from testing import localisation_inference
 def training(params, training_name):
     model = YOLO('yolov8s.pt')
     model.train(**params)
-    localisation_inference(model, params, training_name)
+    # localisation_inference(model, params, training_name)
