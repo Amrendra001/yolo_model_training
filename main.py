45cabd7ae097747b9fddca24f87c167633b3e44f
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-05 10:46:00 +0530

    Update

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def download_training_set():
 if __name__ == '__main__':
 
     project_name = 'Testing_2'
-    training_name = '500 epochs'
+    training_name = 'Testing'
 
     download_training_set()
     download_test_data()
 
     params = {
         'data': "data.yaml",
-        'epochs': 500,
-        'save_period': 100,
+        'epochs': 1,
+        # 'save_period': 100,
         'save': False,
         'batch': 32,
         'single_cls': True,
