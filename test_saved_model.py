06311d318d6443b91544f03baa0601ab4e95c01f
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-15 15:31:05 +0530

    Update

--- test_saved_model.py
+++ test_saved_model.py
@@ -5,10 +5,10 @@ from testing import localisation_inference
 
 if __name__ == '__main__':
 
-    project_name = 'Yolov8_200'
-    training_name = 'yolov8'
+    project_name = 'Testing_new_2'
+    training_name = 'yolov8l_augment2'
 
     ls = glob(f'{project_name}/{training_name}/weights/*.pt')
     for path in ls:
         model = YOLO(path)
-        # localisation_inference(model, path, training_name)
+        localisation_inference(model, path, training_name)
