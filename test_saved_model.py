b9dc940bfe38e6233aebba31c9c0cde07578ca74
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-16 09:18:40 +0530

    Update

--- test_saved_model.py
+++ test_saved_model.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     project_name = 'Testing_new_2'
     training_name = 'yolov8l'
 
-    ls = glob(f'{project_name}/{training_name}/weights/*.pt')
+    ls = glob(f'DETR/weights/*.pt')
     for path in ls:
         model = YOLO(path)
         localisation_inference(model, path, training_name)
