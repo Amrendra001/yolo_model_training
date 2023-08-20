3827fb420a12bf94d0966a44e3430797a7174854
Player0109 <40686356+Player0109@users.noreply.github.com>
2023-08-20 09:06:43 +0530

    Update

--- test_saved_model.py
+++ test_saved_model.py
@@ -1,9 +1,10 @@
 from glob import glob
 from ultralytics import YOLO
-from testing import localisation_inference
+from testing import localisation_inference, download_test_data
 
 
 if __name__ == '__main__':
+    download_test_data()
 
     project_name = 'pypdfium'
     training_name = 'yolov8m'
